3d1bd3331fb8fc91606e32688a9fa92276b77ad4
Winking-maniac <gkgim@yandex.ru>
2022-07-26 09:47:45 +0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,24 @@
+def get_forecast(latitude, longtitude, offset):
+    """
+    Returns string with forecast given to user
+    """
+    return "18 C, 8м/с"
+    
+def get_forecast_from_api(latitude, longtitude, offset):
+    """ request.get ... """
+    return ""
+
+def get_temp_from_json(json_weather):
+    return ""
+
+def get_cloud_from_json(json_weather):
+    return ""
+
+def get_rain_from_json(json_weather):
+    return ""
+
+def get_wind_from_json(json_weather):
+    return ""
+
+def get_sunrise_from_json(json_weather):
+    return ""
